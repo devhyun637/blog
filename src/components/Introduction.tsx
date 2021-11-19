1076bbc57e1fd54ea61b90cf358c087784d21ea0
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-11-19 23:22:45 +0900

    feat: 메인페이지 소개 부분 컴포넌트 구현

---
+++ src/components/Introduction.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "@emotion/styled";
+import ProfileImage from "components/ProfileImage";
+
+const Background = styled.div`
+  width: 100%;
+  padding: 2rem;
+
+  background-color: #ff9d94;
+  color: #ffffff;
+`;
+
+const Wrapper = styled.div`
+  margin: 0 auto;
+
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+`;
+
+const SubTitle = styled.div`
+  font-size: 20px;
+  font-weight: 400;
+`;
+
+const Title = styled.div`
+  margin-top: 5px;
+  margin-bottom: 45px;
+
+  font-size: 30px;
+  font-weight: 700;
+`;
+
+const Introduction = () => {
+  return (
+    <Background>
+      <Wrapper>
+        <div>
+          <SubTitle>은현초 개발블로그</SubTitle>
+          <Title>머릿속에 떠다니는 생각과 지식을 정리합니다.</Title>
+        </div>
+
+        <ProfileImage />
+      </Wrapper>
+    </Background>
+  );
+};
+
+export default Introduction;
---
+++ src/components/ProfileImage.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const PROFILE_IMAGE_LINK =
+  "https://user-images.githubusercontent.com/59258239/142620313-da722e51-376b-4618-9962-0c142cc3eba3.jpeg";
+
+const ProfileImageWrapper = styled.div`
+  width: 150px;
+  height: 150px;
+  margin: 30px;
+
+  background-image: url(${PROFILE_IMAGE_LINK});
+  background-position: 10%;
+  background-repeat: no-repeat;
+  background-size: cover;
+  border-radius: 20%;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.15), 0 2px 2px rgba(0, 0, 0, 0.15),
+    0 4px 4px rgba(0, 0, 0, 0.15), 0 8px 8px rgba(0, 0, 0, 0.15);
+`;
+
+const ProfileImage = () => {
+  return <ProfileImageWrapper />;
+};
+
+export default ProfileImage;
