eff8a763fa35143b836be0bcddf2b10050ad9029
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-11-19 23:21:27 +0900

    feat: 글로벌 스타일 파일 생성

---
+++ src/components/GlobalStyle.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Global, css } from "@emotion/react";
+
+const defaultStyle = css`
+  @font-face {
+    font-family: "Pretendard-Regular";
+    src: url("https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff")
+      format("woff");
+  }
+
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: "Pretendard-Regular", serif;
+  }
+
+  html,
+  body,
+  #___gatsby {
+    width: 100%;
+    height: 100%;
+  }
+
+  a,
+  a:hover {
+    color: inherit;
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  ul,
+  ol {
+    list-style: none;
+  }
+`;
+
+const GlobalStyle = () => {
+  return <Global styles={defaultStyle} />;
+};
+
+export default GlobalStyle;
